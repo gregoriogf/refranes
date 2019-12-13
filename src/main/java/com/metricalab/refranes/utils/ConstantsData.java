a04094642350f49e1e72e94a6159d06ad9d7490c
Jose Franco Nieto <veganum@hotmail.com>
2019-12-13 12:21:28 +0100

    Creadas constantes asc,desc y sb_memory

--- src/main/java/com/metricalab/refranes/utils/ConstantsData.java
+++ src/main/java/com/metricalab/refranes/utils/ConstantsData.java
@@ -5,4 +5,8 @@ public class ConstantsData {
 	public static final String CODE_ERR_SEARCH_REFRAN = "REF001";
 	public static final String MESS_ERR_SEARCH_REFRAN = "Id no encontrado en la base de datos.";
 
+	public static final String ASCENDENTE = "ASC";
+	public static final String DESCENDENTE = "DESC";
+
+	public static final String SPRING_BOOT_MEMORY = "sb_memory";
 }
